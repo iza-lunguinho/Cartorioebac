38896494c3b3cd517c795e9b7673f8bc4425ece1
iza-lunguinho <126526067+iza-lunguinho@users.noreply.github.com>
2023-03-07 19:19:53 -0300

    Opção saida
    Incluso no sistema a opção saída para o usuário

--- Cartório.C3 (1).cpp
+++ Cartório.C3 (1).cpp
@@ -129,6 +129,7 @@ int main ()
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - Consultar nomes\n");
 	    printf("\t3 - Deletar nomes\n\n");
+	    printf("\t4 - Sair do sistema\n\n");
 	    printf("Opcão:"); //fim do menu
 	
         scanf("%d" , &opcao); //armazenando a esolha do usuário 
@@ -149,6 +150,12 @@ int main ()
 			case 3:
 			deletar();
 			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistemaa\n");
+			return 0;
+			break; 
+			
 			
 			default:
 			printf("Essa opção não esta disponivel!\n");	
